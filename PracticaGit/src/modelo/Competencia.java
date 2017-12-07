c2e6a47c50f6c82541a3607c243c67c65f69b970
FREDDY <FREDDY@172.16.222.210>
2017-12-08 09:49:08 +1100

    get

--- PracticaGit/src/modelo/Competencia.java
+++ PracticaGit/src/modelo/Competencia.java
@@ -4,6 +4,24 @@ public class Competencia {
 	private String nombre;
 	private String tipo;
 	private String lugar;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getLugar() {
+		return lugar;
+	}
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
 	
 
 
