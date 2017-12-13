9636efab6f35902b6aadd469d74fbd1de281c905
ADRIAN2 <ADRIAN2@usuario>
2017-12-12 20:11:51 -0500

    Cambios

--- PracticaGit/src/modelo/Articulo.java
+++ PracticaGit/src/modelo/Articulo.java
@@ -4,5 +4,35 @@ public class Articulo {
 	private String titulo;
 	private String pagInicio;
 	private String pagFinal;
+	public Articulo(String titulo, String pagInicio, String pagFinal) {
+		super();
+		this.titulo = titulo;
+		this.pagInicio = pagInicio;
+		this.pagFinal = pagFinal;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getPagInicio() {
+		return pagInicio;
+	}
+	public void setPagInicio(String pagInicio) {
+		this.pagInicio = pagInicio;
+	}
+	public String getPagFinal() {
+		return pagFinal;
+	}
+	public void setPagFinal(String pagFinal) {
+		this.pagFinal = pagFinal;
+	}
+	@Override
+	public String toString() {
+		return "Articulo [titulo=" + titulo + ", pagInicio=" + pagInicio + ", pagFinal=" + pagFinal + "]";
+	}
+	
+	
 	
 }
--- PracticaGit/src/modelo/Autor.java
+++ PracticaGit/src/modelo/Autor.java
@@ -4,5 +4,34 @@ public class Autor {
 private String nombre;
 private String fecNac;
 private String nacionalidad;
+public Autor(String nombre, String fecNac, String nacionalidad) {
+	super();
+	this.nombre = nombre;
+	this.fecNac = fecNac;
+	this.nacionalidad = nacionalidad;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getFecNac() {
+	return fecNac;
+}
+public void setFecNac(String fecNac) {
+	this.fecNac = fecNac;
+}
+public String getNacionalidad() {
+	return nacionalidad;
+}
+public void setNacionalidad(String nacionalidad) {
+	this.nacionalidad = nacionalidad;
+}
+@Override
+public String toString() {
+	return "Autor [nombre=" + nombre + ", fecNac=" + fecNac + ", nacionalidad=" + nacionalidad + "]";
+}
+
 
 }
